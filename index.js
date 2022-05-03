affce9e78636112ae7d8e91b1e270f6324a3aca4
Peapablo <puvadachpp@hotmail.com>
2022-05-03 15:02:57 +0700

    B06 Express Router

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = 3000;
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
+const dishRouter = require('./routes/dishRouter');
 
+app.use('/dishes', dishRouter);
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
